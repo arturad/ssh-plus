9b6fd66ef0a1a29a77608baabba74766d2d3064c
arturad <51878554+arturad@users.noreply.github.com>
2026-05-22 22:56:04 +0200

    Update script URL in setup-pdirect.sh

--- setup-pdirect.sh
+++ setup-pdirect.sh
@@ -937,7 +937,7 @@ menu
 clear
 echo "Atnaujinamas skriptas..."
 
-bash <(curl -Ls https://raw.githubusercontent.com/arturad/ssh-plus/main/setup.sh)
+bash <(curl -Ls https://raw.githubusercontent.com/arturad/ssh-plus/main/setup-pdirect.sh)
 ;;
     14)
         clear
